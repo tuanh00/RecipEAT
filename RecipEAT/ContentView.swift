05b01b776cf62699c365cf7002f22776e89a305b
TuAnh <june265zz@gmail.com>
2025-02-17 02:01:10 -0500

    Initial Commit

---
+++ RecipEAT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecipEAT
+//
+//  Created by LaSalle on 2025-02-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecipEAT/RecipEATApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipEATApp.swift
+//  RecipEAT
+//
+//  Created by LaSalle on 2025-02-17.
+//
+
+import SwiftUI
+
+@main
+struct RecipEATApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
